15a4f63e18ac789309fce9955873d56bcb3f2e41
John Mendes <johnhenrymendes@gmail.com>
2018-05-26 10:15:49 -0400

    feature: added a new method called displayMonth which creates a date object and displays the current month and year in the UI

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ var UIController = (function(){
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container', 
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage', 
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -286,6 +287,20 @@ var UIController = (function(){
         });
     },
 
+    displayMonth: function() {
+
+        var now, year, month, months;
+
+         now = new Date();
+         month = now.getMonth();
+         months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+        'October', 'November', 'December'];
+        // var christmas = new Date(2018, 11, 25);
+         year = now.getFullYear();
+         document.querySelector(DOMstrings.dateLabel).textContent = `${months[month]} ${year}`;
+
+    },
+
         getDOMstrings: function() {
             return DOMstrings;
     }
@@ -387,6 +402,7 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function() {
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
